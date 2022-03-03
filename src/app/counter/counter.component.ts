ec0fc48880a9c0f1ba8d7d194c7d23d1a3bccddf
charl0tte <karolina.kozak@cisc.com.pl>
2022-03-03 14:58:01 +0100

    date format works

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,5 +1,13 @@
+import { NodeWithI18n } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import * as dayjs from 'dayjs'
+import { Dayjs } from 'dayjs';
+import * as relativeTime from 'dayjs/plugin/relativeTime'
+import * as duration from 'dayjs/plugin/duration'
 
+const theDate = dayjs('2022-06-01 00:00:00');
+dayjs.extend(relativeTime);
+dayjs.extend(duration);
 @Component({
   selector: 'app-counter',
   templateUrl: './counter.component.html',
@@ -7,9 +15,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CounterComponent implements OnInit {
 
+  difference: any;
+  interval: any;
+  now: any;
+  days: any;
+
+  // .format('M DD HH:mm:ss')
+
   constructor() { }
 
   ngOnInit(): void {
+
+    this.interval = setInterval(() => {
+
+      this.now = dayjs().format('YYYY-MM-DD HH:mm:ss')
+      this.difference = theDate.diff(this.now);
+      this.days = dayjs.duration(this.difference).asDays().toFixed(0);
+
+    }, 1000);
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.interval);
   }
 
 }
